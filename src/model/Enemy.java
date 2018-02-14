0e8bfcb9e4447bc36136e0bf8bdd1777cce0cd7c
superzaky <superkytoz@gmail.com>
2018-02-14 22:08:26 +0100

    Implement a Following AI.

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -7,25 +7,48 @@ import robotgame.StartingClass;
 public class Enemy {
     private int power, speedX, centerX, centerY;
     private Background bg = StartingClass.getBg1();
+    private Robot robot = StartingClass.getRobot();
     
     public Rectangle r = new Rectangle(0,0,0,0);
-    
     public int health = 5;
+
+    private int movementSpeed;
 
     // Behavioral Method
     public void update() {
+        follow();
         centerX += speedX;
-        speedX = bg.getSpeedX()*5;
+        speedX = bg.getSpeedX() * 5 + movementSpeed;
         r.setBounds(centerX - 25, centerY-25, 50, 60);
-        
+
         if (r.intersects(Robot.yellowRed)){
             checkCollision();
         }
     }
-    
+
     private void checkCollision() {
         if (r.intersects(Robot.rect) || r.intersects(Robot.rect2) || r.intersects(Robot.rect3) || r.intersects(Robot.rect4)){
             System.out.println("collision");
+        }
+    }
+
+    public void follow() {
+
+        if (centerX < -95 || centerX > 810) {
+            movementSpeed = 0;
+        }
+
+        else if (Math.abs(robot.getCenterX() - centerX) < 5) {
+            movementSpeed = 0;
+        }
+
+        else {
+
+            if (robot.getCenterX() >= centerX) {
+                movementSpeed = 1;
+            } else {
+                movementSpeed = -1;
+            }
         }
     }
 
